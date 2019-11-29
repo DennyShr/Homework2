d0aed3eadcb43cc11f723d8d91d0ff4a27857960
DennyShr <messer93@gmail.com>
2019-11-29 22:55:52 +0200

    aaa

--- ForPizza/pizza/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class Feedback(forms.Form):
-	name = forms.CharField(label='Your name:', max_length=100)
-	feedback = forms.CharField(label='Your feedback:', widget=forms.Textarea, max_length=256)
-
--- ForPizza/pizza/urls.py
+++ ForPizza/pizza/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views import PizzasList, PizzaTemplateView, MyView, PizzaOrderCreate, FeedbackView
+from .views import PizzasList, PizzaTemplateView, MyView
 
 urlpatterns = [
 	path('pizzas/', PizzasList.as_view()),
 	path('', PizzaTemplateView.as_view()),
-	path('feedback/', FeedbackView.as_view()),
 	path('view/', MyView.as_view()),
-	path('pizza_order/add/', PizzaOrderCreate.as_view(), name='order-add'),
 ]
