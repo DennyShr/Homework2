1e297c4756e252697ca70341e926746dbbbfe9a1
DennyShr <messer93@gmail.com>
2019-11-29 23:03:08 +0200

    new

--- ForPizza/pizza/views.py
+++ ForPizza/pizza/views.py
@@ -1,10 +1,7 @@
 from django.views.generic import ListView, TemplateView
-from django.views.generic.edit import FormView, CreateView
 from django.views import View
 from .models import PizzaName, PizzaOrdered
 from django.http import HttpResponse
-from .forms import Feedback
-from django.urls import reverse_lazy
 
 class PizzasList(ListView):
 	model = PizzaName
@@ -12,16 +9,6 @@ class PizzasList(ListView):
 
 class PizzaTemplateView(TemplateView):
 	template_name = 'pizza/pizza_template.html'
-
-class FeedbackView(FormView):
-	template_name = 'pizza/feedback.html'
-	form_class = Feedback
-	success_url = '/pizza/pizzas/'
-
-class PizzaOrderCreate(CreateView):
-	template_name = 'pizza/pizza_order.html'
-	model = PizzaOrdered
-	fields = ['pizza', 'side_name', 'additional_sauce', 'number']
 
 class MyView(View):
 	def get(self, request):
